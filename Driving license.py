716d6d42ed6f998c79ff61042d95466ff24bf1c9
ganesh23b8-cmyk <ganesh23b8@gmail.com>
2025-10-26 12:45:11 +0530

    Program 2

---
+++ Driving license.py
@@ -0,0 +1,6 @@
+name=input("enter the name")
+age=int(input("enter the age"))
+if(age>=18):
+    print("you are eligible to apply for driving license")
+else:
+    print("you are not eligible to apply for driving license")
