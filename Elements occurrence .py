36b130bae27135c0a9931dad52fb8009ccf6d9fa
ganesh23b8-cmyk <ganesh23b8@gmail.com>
2025-11-10 20:16:31 +0530

    Program10

---
+++ Elements occurrence .py
@@ -0,0 +1,5 @@
+list1=[10,20,40,50,60,20,50,10,30,50,30,24,45,60,70,20,30,30,50,70,70,50,60]
+print("the list",list1)
+num=int(input("which element occurrence you would like to count?:"))
+count=list1.count(num)
+print("the count of eliments is",count)
